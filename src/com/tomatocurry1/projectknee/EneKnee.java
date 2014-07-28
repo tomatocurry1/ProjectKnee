0fb577141d5389ca8d6ddec67c1f5e437f0c364f
tomatocurry1 <lee123456771@gmail.com>
2014-07-27 21:25:20 -0400

    secondFirst?

--- src/com/tomatocurry1/projectknee/EneKnee.java
+++ src/com/tomatocurry1/projectknee/EneKnee.java
@@ -1,10 +1,11 @@
 package com.tomatocurry1.projectknee;
 
+import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.app.Activity;
 import android.view.Menu;
+import android.view.MenuItem;
 
-public class EneKnee extends Activity {
+public class EneKnee extends ActionBarActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -19,4 +20,15 @@ public class EneKnee extends Activity {
 		return true;
 	}
 
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
 }
